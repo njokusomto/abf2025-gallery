bed1b7d86482daef5f6269f5a2c4150dd4061c0b
njokusomto <njokusomto@gmail.com>
2025-11-12 21:03:42 +0200

    Included images.ts

---
+++ utils/images.ts
@@ -0,0 +1,27 @@
+import type { ImageProps } from "./types";
+
+// Replace these URLs with your actual Cloudinary links
+// (the ones that start with https://res.cloudinary.com/abf2025/...)
+const images: ImageProps[] = [
+  {
+    id: 0,
+    public_id: "ABF_2025_Day_two-02054_d5xmhh",
+    format: "jpg",
+    blurDataUrl: "",
+    width: 1280,
+    height: 720,
+    secure_url: "https://res.cloudinary.com/abf2025/image/upload/c_scale,w_1280/ABF_2025_Day_two-02054_d5xmhh.jpg",
+  },
+  {
+    id: 1,
+    public_id: "ABF_2025_Day_two-02055_oybybz",
+    format: "jpg",
+    blurDataUrl: "",
+    width: 1280,
+    height: 720,
+    secure_url: "https://res.cloudinary.com/abf2025/image/upload/c_scale,w_1280/ABF_2025_Day_two-02055_oybybz.jpg",
+  },
+  // 👇 Add as many as you need
+];
+
+export default images;
