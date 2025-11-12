cd4745f3bb0e0f523cae1335c3c44aa9686ea3d5
njokusomto <njokusomto@gmail.com>
2025-11-13 00:07:36 +0200

    Optimised NextJS

--- next.config.js
+++ next.config.js
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-
-  // Tell Next.js that Cloudinary is a trusted external image domain
   images: {
     domains: ["res.cloudinary.com"],
-    // Disable the built-in optimizer for all Cloudinary URLs
     unoptimized: true,
   },
-
-  // Optional: improve performance for static export
-  experimental: {
-    optimizeCss: true,
-  },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
