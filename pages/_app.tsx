eddf4272074c080b868d620b21549e6e86ce3e21
njokusomto <njokusomto@gmail.com>
2025-11-25 02:48:58 +0000

    Refactor code structure for improved readability and maintainability

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
